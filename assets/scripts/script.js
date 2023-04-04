9387cc3bfd5d72f23888c4c37fabb09918596fd6
Dominica-E-Purcell <dominica.e.purcell@gmail.com>
2023-04-03 23:21:42 -0400

    javascript

---
+++ assets/scripts/script.js
@@ -0,0 +1,27 @@
+var citySearchElement = getElementById('current-search-results')
+var previousSearchElement =  getElementById('previous-searches')
+var newSearchElement =  getElementById('new-search')
+var cityInput = [{lat}, {lon}];
+
+function searchResults() {
+fetch(api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={b42218bf8657ef7bf4e7e9c2b8885fb6}&list.main.temp.units=imperial&list.main.humidity&list.wind.speed.units=imperial)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    (data).value
+        if(localStorage.getItem('previous-searches') === null) {
+            localStorage.setItem('previous-searches', '[]');
+            var previousSearchElement = JSON.parse(localStorage.getItem('previous-searches'));
+            previousSearchElement.push(newSearchElement);
+            localStorage.setItem('previous-searches', JSON.stringify(previousSearchElement));
+        if (localStorage.getItem('previous-searches') !== null) {
+                document.getElementById('new-search').innerHTML = JSON.parse(localStorage.getItem('current-search-results'))
+        return;
+                }
+            }
+        }
+    );
+}
+ 
